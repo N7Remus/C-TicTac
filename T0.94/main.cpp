07abca69918834826c86069042c180cef75d2722
N7Remus <remaigabor@gmail.com>
2019-04-15 20:56:46 +0200

    Update main.cpp
    update working, but not with ncurses

--- T0.94/main.cpp
+++ T0.94/main.cpp
@@ -33,8 +33,6 @@ public:
     Node* node_next;
     Node* node_prew;
 
-    //    Node();
-
     Node() {
         this->subs_length_pos = 0;
         this->subs_length_neg = 0;
@@ -62,32 +60,6 @@ public:
         this->subs_length_neg++;
     }
 
-    int getData(int y) {
-        if (y < 0) {
-            SubNode* tail = this->subnode_tail;
-            int i = 0;
-            while (i != abs(y)) {
-                tail = tail->subnode_prew;
-                i++;
-            }
-            return tail->data;
-            //                return tail->data; // == abs(y);
-        } else {
-            SubNode* head = this->subnode_head;
-            int i = 0;
-            while (i != abs(y)) {
-                //                    cout << "node2";
-
-                head = head->subnode_next;
-                i++;
-            }
-            if (head)
-                return head->data;
-
-            //                return head->data; // == abs(y);
-        }
-        return 0;
-    }
     //    void print() {
     //        SubNode* head = this->subnode_head;
     //        int i = 1;
@@ -136,17 +108,13 @@ public:
     }
 
     void addNewNodes(int x, int y) {
-        /*                                eddig töltöm fel
-                                          negatív<->positive szám
-         */
-
         if (x < 0) {
             do {
-                addToTail(y);
+                this->addToTail(y);
             } while (this->length_neg <= abs(x));
         } else {
             do {
-                addToHead(y);
+                this->addToHead(y);
             } while (this->length_pos <= abs(x));
 
         }
@@ -184,27 +152,6 @@ public:
         this->length_neg++;
     }
 
-    int getData(int x, int y) {
-        if (x < 0) {
-            Node* tail = this->node_tail;
-            int i = 0;
-            while (i != abs(x)) {
-                tail = tail->node_prew;
-                i++;
-            }
-            return tail->getData(y); //== abs(x);
-        } else {
-            Node* head = this->node_head;
-            int i = 0;
-            while (i != abs(x)) {
-                head = head->node_next;
-                i++;
-            }
-            return head->getData(y); //== abs(x);
-        }
-        //        return 0;
-    }
-
 
     //    void print() {
     //        Node* head = this->node_head;
@@ -240,45 +187,71 @@ int getValue(int x, int y) {
     Node* node;
     SubNode* subnode;
     int i = 0;
+    int j = 0;
     list->addNewNodes(x, y);
 
     if (x < 0) {
         node = list->node_tail;
+        while (subnode) {
+            if (i == abs(x))
+                break;
+            node = node->node_prew;
+            i++;
+        }
     } else {
         node = list->node_head;
+        while (subnode) {
+            if (i == abs(x))
+                break;
+            node = node->node_next;
+            i++;
+        }
     }
     if (y < 0) {
         subnode = node->subnode_tail;
         while (subnode) {
-            if (i == abs(y))
+            if (j == abs(y))
                 //                return subnode->data;
                 break;
             subnode = subnode->subnode_prew;
-            i++;
+            j++;
         }
     } else {
         subnode = node->subnode_head;
         while (subnode) {
-            if (i == abs(y))
+            if (j == abs(y))
                 break;
             subnode = subnode->subnode_next;
-            i++;
+            j++;
         }
     }
     return subnode->data;
     //    return 0;
 }
 
-int setValue(int x, int y, int data) {
+void setValue(int x, int y, int data) {
     Node* node;
     SubNode* subnode;
     int i = 0;
+    int j = 0;
     list->addNewNodes(x, y);
 
     if (x < 0) {
         node = list->node_tail;
+        while (subnode) {
+            if (i == abs(x))
+                break;
+            node = node->node_prew;
+            i++;
+        }
     } else {
         node = list->node_head;
+        while (subnode) {
+            if (i == abs(x))
+                break;
+            node = node->node_next;
+            i++;
+        }
     }
     if (y < 0) {
         subnode = node->subnode_tail;
@@ -307,99 +280,64 @@ int main(int argc, char const *argv[]) {
     int vision = 5;
     int c = 0;
     int x = 1;
-    //    list->addNewNodes(0, 10);
-    //    cout << "Size: LL : " << list->length_pos << endl;
-    //    cout << "data : " << list->getData(0, 0) << endl;
-    //    cout << "Size SL : " << list->node_head->subs_length_pos << endl;
-    //    SubNode* head = list->node_head->subnode_head;
-    //    int i = 0;
-    //    while (head) {
-    //        cout << head->data;
-    //        head = head->subnode_next;
-    //        i++;
-    //    }
-    //    cout << endl << "i:" << i << endl;
-    //
-    //    list->addNewNodes(0, 101);
-    //    cout << "Size: LL : " << list->length_pos << endl;
-    //    cout << "data : " << list->getData(0, 0) << endl;
-    //    cout << "Size SL : " << list->node_head->subs_length_pos << endl;
-    //    head = list->node_head->subnode_head;
-    //    i = 0;
-    //    while (head) {
-    //        cout << head->data;
-    //        head = head->subnode_next;
-    //        i++;
-    //    }
-    //    cout << endl << "i:" << i;
-    //
-    //    list->addNewNodes(0, -10);
-    //    cout << "data : " << getValue(0, -1) << endl;
+    int v;
+    if (0) {
+//        cout << getValue(-5, -2);
+        cout << getValue(-5, 0);
+        cout << getValue(0, -2);
+        cout << getValue(0, 0);
+        cout << getValue(-5, 2);
+    } else {
+//        initscr();
+//        noecho();
+//        raw();
+//        keypad(stdscr, TRUE);
+//        move(0, 0);
+//        printw("Kilepeshez (ESC): ");
+//        do {
+            if (c == 97 || c == 260)
+                current_pos[0] = current_pos[0] - 1;
+            /*  w  or up */
+            if (c == 119 || c == 259)
+                current_pos[1] = current_pos[1] + 1;
+            /*  d  or right */
+            if (c == 100 || c == 261)
+                current_pos[0] = current_pos[0] + 1;
+            /*  s or down  */
+            if (c == 115 || c == 258)
+                current_pos[1] = current_pos[1] - 1;
+            /* space*/
+            if (c == 32) {
+                //            return 0;
+                setValue(current_pos[0], current_pos[1], 1);
+            }
+            x = 1;
+            for (int i = current_pos[0] - vision;
+                    i < current_pos[0] + vision + 1;
+                    i++) {
+//                move(x, 0);
+//                refresh();
 
-    //    list->addNewNodes(0, 1);
-    //    cout << "Size LL: "<<list->length_pos << endl;
+                for (int j = current_pos[0] - vision;
+                        j < current_pos[0] + vision + 1;
+                        j++) {
+                    //                    list->addNewNodes(i, j);
+                    //                    cout<<list->getData(i,j);
+                    v = getValue(i, j);
+                    cout << v;
+//                    printw("%d", v);
 
-
-    //    list->addNewNodes(0, 3);
-    //    cout << list->getData(0, 2);
-
-    initscr();
-    noecho();
-    raw();
-    keypad(stdscr, TRUE);
-    move(0, 0);
-    printw("Kilepeshez (ESC): ");
-    do {
-        if (c == 97 || c == 260)
-            current_pos[0] = current_pos[0] - 1;
-        /*  w  or up */
-        if (c == 119 || c == 259)
-            current_pos[1] = current_pos[1] + 1;
-        /*  d  or right */
-        if (c == 100 || c == 261)
-            current_pos[0] = current_pos[0] + 1;
-        /*  s or down  */
-        if (c == 115 || c == 258)
-            current_pos[1] = current_pos[1] - 1;
-        /* space*/
-        if (c == 32) {
-            //            return 0;
-            setValue(current_pos[0], current_pos[1], 1);
-        }
-        x = 1;
-        for (int i = current_pos[0] - vision;
-                i < current_pos[0] + vision + 1;
-                i++) {
-            move(x, 0);
-            refresh();
-
-            for (int j = current_pos[0] - vision;
-                    j < current_pos[0] + vision + 1;
-                    j++) {
-                list->addNewNodes(i, j);
-                //                    cout<<list->getData(i,j);
-                printw("%d", getValue(i, j));
-                //                            cout << get(i, j);
+                }
+                //            cout << "\n";
+                x++;
 
             }
-            //            cout << "\n";
-            x++;
-
-        }
-        move(x + vision + 1, 0);
-        printw("%d ; %d", current_pos[0], current_pos[1]);
-        move(vision + 1, vision);
-    } while ((c = getch()) != 27 && !win);
-    endwin();
-    //
-    //        for (int i = 0; i < 100; ++i) {
-    //            list->addToHead(i);
-    //            list->addToTail(100 - i);
-    //        }
-    //        list->print();
-    //        list->print_neg();
-    //        std::cout << "List Length: -" << list->length_neg << std::endl;
-    //        std::cout << "List Length: +" << list->length_neg << std::endl;
+//            move(x + vision + 1, 0);
+//            printw("%d ; %d", current_pos[0], current_pos[1]);
+//            move(vision + 1, vision);
+//        } while ((c = getch()) != 27 && !win);
+//        endwin();
+    }
     delete list;
     return 0;
 }
