7fcf3fd6c3ab8dcdc2a30955b501dac816477a35
N7Remus <remaigabor@gmail.com>
2019-04-18 13:01:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,62 +7,43 @@
 
 #include <cstdlib>
 #include <iostream>
-//#include <ncurses.h>
-//#include <curses.h>
+#include <ncurses.h>
+#include <curses.h>
 using namespace std;
+int cursor_X = 0;
+int cursor_y = 0;
 
-/*
-333333333
-111111111    main tömb első szinjének alszintjei
-864213579   main tömb első szintje 
-222222222
-555555555
- */
-
-int** expandBoard(int** array) {
- 
-    int arraySize = array[0][0];
-    int** newArray;
-    newArray = new int*[arraySize + 1];
-    newArray[0] = new int[1];
-    newArray[0][0] = arraySize + 1;
-    for (int i = 1; i < array[0][0]+1; i++) {
-        //                      subarraySize
-        newArray[i] = new int[array[i][0]];
-        newArray[i][0] = array[i][0] ;
-        for (int j = 1; j < array[i][0]; j++) {
-            newArray[i][j] = array[i][j];
+char** initBoard(int row, int col) {
+    char** newChar = new char*[row];
+    for (int i = 0; i < row; i++) {
+        newChar[i] = new char[col];
+        for (int j = 0; j < col; j++) {
+            newChar[i][j] = '.';
         }
     }
-    array = newArray;
-    delete newArray;
-    return array;
+    return newChar;
+}
+
+char** expandBoard(int row, int col, char key_press,
+        char ** original_board) {
+    /* 
+     keypress col row bővítés ha fájlszélén
+     * 
+     */
+    char ** newChar = initBoard(row, col);
+
+    return newChar;
 }
 
 int main(int argc, char** argv) {
-    int** main_array;
-    int arraySize = 5;
-    int subarraySize = 5;
-    main_array = new int*[arraySize];
-    //    main_array[0] = arraySize;
-    main_array[0] = new int[1];
-    main_array[0][0] = arraySize;
-    for (int i = 1; i < main_array[0][0]; ++i) {
-        main_array[i] = new int[subarraySize];
-        main_array[i][0] = subarraySize;
-    }
-    for (int i = 1; i < main_array[0][0]; i++) {
-        for (int j = 1; j < main_array[i][0]; j++) {
-            main_array[i][j] = i;
+    int row = 5;
+    int col = 4;
+    char** main_array = initBoard(row, col);
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
             cout << main_array[i][j];
         }
-    }
-    expandBoard(main_array);
-    for (int i = 1; i < main_array[0][0]; i++) {
-        for (int j = 1; j < main_array[i][0]; j++) {
-            main_array[i][j] = i;
-            cout << main_array[i][j];
-        }
+        cout << "\n";
     }
 
     return 0;
