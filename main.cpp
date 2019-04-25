82e66da1c53cd8011081f55d8d398943b90eab2e
N7Remus <remaigabor@gmail.com>
2019-04-26 00:29:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+
+#include "pch.h"
+#include <iostream>
 #include <iostream>
 #include <windows.h>
 #include<conio.h>
@@ -25,7 +28,7 @@ int cursor_X = 1;
 int cursor_Y = 1;
 
 char** initBoard(int row, int col) {
-	char** newCharArray = new char* [row];
+	char** newCharArray = new char*[row];
 	for (int i = 0; i < row; i++) {
 		newCharArray[i] = new char[col];
 		for (int j = 0; j < col; j++) {
@@ -35,50 +38,50 @@ char** initBoard(int row, int col) {
 	return newCharArray;
 }
 
-char** cloneArrayValues(int row_from ,int row_to , int col_from, int col_to, char** original_board, char** new_board) {
+char** cloneArrayValues(int row, int col, int row_extra, int col_extra, char** original_board) {
+
+
 	/* Lemásolja a orignal array értékeit */
-	char** newCharArray = new_board;
-	for (int i = row_from; i < row_to; i++) {
-		for (int j = col_from; j < col_to; j++) {
-			newCharArray[i][j] << original_board[i][j];
-		}
-
-	}
-	return newCharArray;
-}
-
-char** expandBoard(int row_orig, int col_orig, char** original_board,bool left,bool down) {
-	/*
-	* keypress col row bővítés ha a pálya szélén van
-	*/
-	int row;
-	int col;
-	
-	row = row_orig;
-	col = col_orig;
-	//új tömb
 	char** newCharArray = initBoard(row, col);
-	//cloneArrayValues(row_from, row_to, col_from, col_to, original_board, newCharArray);
-
+	if (row_extra < 0) {
+		row--; 
+		row_extra = 0;
+	}
+	else 
+		row--;
+	
+	if (col_extra < 0) {
+		col--;
+		col_extra = 0;
+	}
+	else
+		col--;
+	for (int i = row_extra; i < row; i++) {
+		for (int j = col_extra; j < col; j++) {
+			newCharArray[i][j] = original_board[i][j];
+		}
+	}
+	delete[] original_board;
 	return newCharArray;
 }
 
 int main(int argc, char** argv) {
 	int row = 3;
 	int col = 3;
-	int vision=2;
+	int vision = 4;
 	char** main_array = initBoard(row, col);
 
 
 	while (true)
 	{
 		system("cls");
-		cout << endl << cursor_X << ":" << cursor_Y<<endl;
+		cout << endl << cursor_X << ":" << cursor_Y << endl;
+		cout << endl << "row : " << row << ":" << " col : " << col << endl;
 
-		for (int i = cursor_X - vision; i < cursor_X + vision + 1; i++) {
-			for (int j = cursor_X - vision; j < cursor_Y + vision + 1; j++) {
+		for (int i = cursor_Y + vision; i > cursor_Y - vision-1; i--) {
+			for (int j = cursor_X - vision; j < cursor_X + vision + 1; j++) {
 				if (i < 0 || j < 0 || row <= i || col <= j) {
-					cout << "-";
+					cout << ":";
 				}
 				else {
 					cout << main_array[i][j];
@@ -88,33 +91,44 @@ int main(int argc, char** argv) {
 		}
 		switch (_getch()) {
 		case KEY_UP:
-			cursor_X--;
-			break;
-		case KEY_DOWN:
-			cursor_X++;
-			break;
-		case KEY_LEFT:
-			cursor_Y--;
-			break;
-		case KEY_RIGHT:
 			cursor_Y++;
 			break;
+		case KEY_DOWN:
+			cursor_Y--;
+			break;
+		case KEY_LEFT:
+			cursor_X--;
+			break;
+		case KEY_RIGHT:
+			cursor_X++;
+			break;
 		case KEY_ENTER:
-			main_array[cursor_X][cursor_Y] = '+';
+			main_array[cursor_Y][cursor_X] = '+';
 			break;
 		}
-		if (cursor_X < 0)
-			cursor_X = 0;
-		if (cursor_X >= col)
-			cursor_X = col - 1;
-		if (cursor_Y < 0)
+		if (cursor_Y < 0){
+			row++;
+			main_array = cloneArrayValues(row, col, 1, 0, main_array);
 			cursor_Y = 0;
-		if (cursor_Y >= row)
-			cursor_Y = row - 1;
+		}
+		if (cursor_Y >= row){
+			row++;
+			main_array = cloneArrayValues(row, col, -1, 0, main_array);
+			
+		}
+		if (cursor_X < 0){
+			col++;
+			main_array = cloneArrayValues(row, col, 0, 1, main_array);
+			cursor_X = 0;
+		}
+		if (cursor_X >= col) {
+			col++;
+			main_array = cloneArrayValues(row, col, 0, -1, main_array);
+			
+		}
 	}
 	cout << "Amoba jatek" << endl;
 	cout << "Jatek kezdesehez nyomj Enter-t" << endl << "Kilepeshez ESC-t" << endl;
-	
+
 	return 0;
 }
-
